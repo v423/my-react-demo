d22f07fe48f1085215f2d58af382d3838b1f51aa
v423 <code423@163.com>
2025-11-23 20:13:10 +0800

    init

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import FinalWebStyleLab from './index.jsx';
+
+// Optional: import a minimal CSS reset (there's no Tailwind in this project by default)
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FinalWebStyleLab />
+  </React.StrictMode>
+);
